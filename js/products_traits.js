b55d7c091be3670be93226847cde58117ac5c7ec
steve <steve@veerman.ca>
2023-02-28 17:46:20 -0500

    Commit of project code for Flow Hackathon

---
+++ js/products_traits.js
@@ -0,0 +1,45 @@
+let products = {
+	'nbatopshot': 'NBA Top Shot',
+	'nflallday': 'NFL All Day',
+	'ufcstrike': 'UFC Strike',
+	'laligagolazos': 'LaLiga Golazos'
+};
+
+let traits = {
+	'nbatopshot': {
+		'media_urls.player': 'Image: Player Name',
+		'media_urls.logos': 'Image: Team Name',
+		'media_urls.jersey': 'Image: Player Number',
+		'full_name': 'Text: Player Name',
+		'team_name': 'Text: Team Name',
+		'player_number': 'Text: Player Number',
+		'player_position': 'Text: Player Position',
+		'draft_team': 'Text: Draft Team',
+		'draft_year': 'Text: Draft Year',
+		'birthdate': 'Text: Birthdate',
+		'birthplace': 'Text: Birthplace'
+	},
+	'nflallday': {
+		'media_urls.image-player': 'Image: Player Name',
+		'media_urls.image-details': 'Image: Team Name',
+		'full_name': 'Text: Player Name',
+		'team_name': 'Text: Team Name',
+		'player_number': 'Text: Player Number',
+		'player_position': 'Text: Player Position'
+	},
+	'ufcstrike': {
+		'media_urls.hero': 'Image: Player Name', // media_urls.player missing for hype
+		'media_urls.scores': 'Image: Player Alternate',
+		'media_urls.legal': 'Image: Player Country',
+		'full_name': 'Text: Player Name'
+	},
+	'laligagolazos': {
+		'media_urls.Front_default': 'Image: Player Name',
+		'media_urls.Details_default': 'Image: Team Name',
+		'full_name': 'Text: Player Name',
+		'player_number': 'Text: Player Number',
+		'team_name': 'Text: Team Name',
+		'player_position': 'Text: Player Position',
+		'player_country': 'Text: Player Country'
+	}
+};
